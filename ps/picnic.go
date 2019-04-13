65c50368fe576e0d6ef17b960b662dd088f45854
Leta <stdev17@gmail.com>
2019-04-13 23:53:32 +0900

    backup

---
+++ ps/picnic.go
@@ -0,0 +1,10 @@
+package ps
+
+import (
+	//"fmt"
+	
+)
+
+func Picnic() {
+
+}
---
+++ ps/picnic_test.go
@@ -0,0 +1,13 @@
+package ps
+
+import (
+	"fmt"
+	"testing"
+	"github.com/stretchr/testify/assert"
+	"github.com/onsi/ginkgo"
+	"github.com/onsi/gomega"
+)
+
+func TestPicnic(t *testing.T) {
+
+}
